de932bc4b9464c0e54e382f4c8c9196570b0cd1d
Evellin Lima <evellinlima01@gmail.com>
2025-05-16 19:50:25 -0300

    feat: aplicacao usando useEffect, axios

--- Programação Dispositivos Moveis/projeto_cestaVegetais/App.js
+++ Programação Dispositivos Moveis/projeto_cestaVegetais/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
-import Cesta from "./src/screens/cestas";
-import mock from "./src/mocks/cesta";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import Cesta from './src/screens/index';
+import mock from './src/mocks/cesta'
 
 export default function App() {
   return (
